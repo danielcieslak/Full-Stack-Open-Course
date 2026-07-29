5a8c5d83505be3557686fd78408a2d513fc31df0
danielcieslak <cieslak.dmoritz@gmail.com>
2026-07-29 21:21:21 +0200

    Ex1.3

--- Part 1/Part1/src/App.jsx
+++ Part 1/Part1/src/App.jsx
@@ -1,27 +1,34 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   const Header = ({ course }) => {
+    (console.log(course))
   return <h1>{course}</h1>
 }
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
       <p>
-        {part1} {exercises1}
+        {part1.name} {part1.exercises}
       </p>
       <p>
-        {part2} {exercises2}
+        {part2.name} {part2.exercises}
       </p>
       <p>
-        {part3} {exercises3}
+        {part3.name} {part3.exercises}
       </p>
     </div>
   )
@@ -37,13 +44,10 @@ const Total = ({ exercises }) => {
       <Header course={course} />
       <Content
         part1={part1}
-        exercises1={exercises1}
         part2={part2}
-        exercises2={exercises2}
         part3={part3}
-        exercises3={exercises3}
       />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   )
 }
