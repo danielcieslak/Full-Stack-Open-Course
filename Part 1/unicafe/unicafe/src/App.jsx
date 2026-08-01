ca2c40760eee1869536fa429fb14dae703f86bfd
danielcieslak <cieslak.dmoritz@gmail.com>
2026-08-01 21:16:10 +0200

    EX1.9: unicafe step 4

--- Part 1/unicafe/unicafe/src/App.jsx
+++ Part 1/unicafe/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 const Statistics = (props) => {
+  if (props.good + props.neutral + props.bad === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>Statistics</h1>
