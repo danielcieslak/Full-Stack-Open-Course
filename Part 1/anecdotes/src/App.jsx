6eb41a90cf3289dc037b380eade2e88ee4e2add9
danielcieslak <cieslak.dmoritz@gmail.com>
2026-08-01 22:33:41 +0200

    EX1.14*: anecdotes step 3

--- Part 1/anecdotes/src/App.jsx
+++ Part 1/anecdotes/src/App.jsx
@@ -23,17 +23,23 @@ const App = () => {
 const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const oneVote = (selected) => {
-    const copy = { ...votes }
+    const copy = [...votes]
     copy[selected] += 1
     setVotes(copy)
   }
 
+  const maxIndex = votes.indexOf(Math.max(...votes))
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <Button handleClick={() => oneVote(selected)} text="vote" />
       <Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="next anecdote" />
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[maxIndex]}</p>
+      <p>has {Math.max(...votes)} votes</p>
     </div>
   )
 }
